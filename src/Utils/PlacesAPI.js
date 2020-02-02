f13d2bdcb55c5a43a6b63c8ab7037f4da78f1839
marcoazzurrini <marco.azzurrini.art@gmail.com>
2020-02-02 15:48:24 +0100

    edit env variable

--- src/Utils/PlacesAPI.js
+++ src/Utils/PlacesAPI.js
@@ -2,6 +2,5 @@ export const fetchPlaces = async (lat, lng) => {
   const url = `/.netlify/functions/places?lat=${lat}&lng=${lng}`;
   let response = await fetch(url, { mode: "no-cors" });
   let data = await response.json();
-  console.log(data);
   return data;
 };
--- src/lambda/places.js
+++ src/lambda/places.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 exports.handler = async (event, context) => {
-  const key = process.env.GOOGLE_API;
+  const key = process.env.REACT_APP_GOOGLE_API;
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location=${event.queryStringParameters.lat},${event.queryStringParameters.lng}&radius=5000&type=restaurant&key=${key}`;
 
   return fetch(url)
