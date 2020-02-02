9a69ff0f9b39672227aa6141d6fbe0811b99c8d1
marcoazzurrini <marco.azzurrini.art@gmail.com>
2020-02-02 16:39:04 +0100

    change fetch end point

--- src/Utils/PlacesAPI.js
+++ src/Utils/PlacesAPI.js
@@ -1,5 +1,5 @@
 export const fetchPlaces = async (lat, lng) => {
-  const url = `http://localhost:3000/.netlify/functions/places?lat=${lat}&lng=${lng}`;
+  const url = `.netlify/functions/places?lat=${lat}&lng=${lng}`;
   let response = await fetch(url);
   let data = await response.json();
   return data;
